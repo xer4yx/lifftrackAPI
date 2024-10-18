9a4be8c5c03f21744bd7aac097c7204384fe1610
xerayx <202111173@fit.edu.ph>
2024-10-18 08:27:27 +0800

    Changes in server root

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ class FormOutput(BaseModel):
 
 
 server_origin = [
-    'http://localhost:8000'
+    'http://localhost:8000',
+    'http://127.0.0.1:8000/'
 ]
 
 server_method = ["PUT", "GET", "DELETE"]
